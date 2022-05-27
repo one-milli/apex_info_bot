9e5b0528aa431ec4d980e81866ffa8f62e3a30a0
ohara <kzAme79@gmail.com>
2022-05-28 03:42:48 +0900

    ショップ時間変更

--- app.py
+++ app.py
@@ -304,7 +304,7 @@ scheduler = BlockingScheduler()
 
 scheduler.add_job(map_rotation, 'cron', minute='0,30')
 scheduler.add_job(craft_rotation, 'cron', hour=18)
-scheduler.add_job(store_info, 'cron', hour='18,21', minute=15)
+scheduler.add_job(store_info, 'cron', hour='18,19,21', minute='0,15,30,45')
 scheduler.add_job(predator, 'cron', hour=15)
 
 # APSchedulerを開始
